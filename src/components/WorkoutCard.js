dd538b72c1acbd28c5b17e317a070e5fa78cc419
Megan Fago <meganlaynefago@gmail.com>
2019-05-06 18:40:26 -0400

    added some margin to the cards.

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react'
-import { Link } from 'react-router-dom';
+import { Card, Grid  } from 'semantic-ui-react'
+import { Link} from 'react-router-dom';
 
 class WorkoutCard extends Component {
     render(){
         console.log("This is the workout", this.props.workout)
         return(
             <Link to={`/workouts/${this.props.workout.id}`} className="item">
-            <div>
-            
+            <div class="card">
                 <Card centered>
                
-                
                 <Card.Content>
                     <Card.Header>{this.props.workout.workout_name}</Card.Header>
                     <Card.Meta> Created By: {this.props.workout.user_name} </Card.Meta>
                     <Card.Description>{this.props.workout.description}</Card.Description>
                 </Card.Content>
                 </Card>
+               
                
             </div>
             </Link>
